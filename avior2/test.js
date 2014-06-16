e423d73c84652e3d80eadbbf7e9bf83baadbc671
Melissa <phoenixemere@gmail.com>
2014-06-16 13:21:32 -0400

    Reorganized the repo files.

---
+++ avior2/test.js
@@ -0,0 +1,18 @@
+//callback example source: https://github.com/maxogden/art-of-node#callbacks
+
+var fs = require('fs')
+var myNumber = undefined
+
+function addOne(callback) {
+  fs.readFile('number.txt', function doneReading(err, fileContents) {
+    myNumber = parseInt(fileContents)
+    myNumber++
+    callback()
+  })
+}
+
+function logMyNumber() {
+  console.log(myNumber)
+}
+
+addOne(logMyNumber)
