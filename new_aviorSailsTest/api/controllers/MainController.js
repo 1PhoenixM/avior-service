edf94cb6cdf57d72ed4cebc576071f0ed8a09fa3
Melissa <phoenixemere@gmail.com>
2014-07-01 17:53:02 -0400

    Debugging controller choice page. It changes the connection property for config/models and the host model, yet host/find is still using Floodlight even when I change it to OpenDaylight. Access at localhost:1337

--- new_aviorSailsTest/api/controllers/MainController.js
+++ new_aviorSailsTest/api/controllers/MainController.js
@@ -1,9 +1,13 @@
 module.exports = {
   setinfo: function (req, res) {
+    console.log(sails.config.models.connection);
     sails.config.models.connection = req.param('connection');
-    //sails.models.host.connection = req.param('connection')   
-    var currentconn = sails.config.models.connection;
-    sails.config.connections.floodlight.hostname = req.param('hostname'); //hardcoded for now
+    console.log(sails.config.models.connection);
+    console.log(sails.models.host.connection);  
+    sails.models.host.connection = "[ '" + req.param('connection') + "' ]";
+      console.log(sails.models.host.connection);
+    //var currentconn = sails.config.models.connection;
+    //sails.config.connections.floodlight.hostname = req.param('hostname'); //hardcoded for now
     console.log('\nSet.');
   }
 };
